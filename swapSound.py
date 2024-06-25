b028c1a03dbe15d50f215888bc4ee898e6ea1ec5
lucasrctc <lucasrctc@gmail.com>
2024-06-25 16:12:40 -0500

    Changed script to swap ports in the same sink, sinse headphone is connected to headphone jack instead of monitor

--- swapSound.py
+++ swapSound.py
@@ -1,66 +1,109 @@
 #!/bin/python3
 import subprocess
 
-sinkStr = subprocess.run(["pacmd", "list-sinks"], capture_output = True)
-sinkStr = sinkStr.stdout.decode('ascii').split("\n")
+ports = subprocess.run(["pactl", "list", "sinks"], capture_output = True)
+
+ports = ports.stdout.decode('ascii').split("\n")
+
+sinkName = ""
+
+begin = 0
+end = 0
+
+for i, txt in enumerate(ports):
+    #print(txt)
+    # Find sink name
+    if "Name:" in txt:
+        sinkName = txt[txt.find(": ") + 2:]
+        #print(sinkName)
+    # Find substring with port, active port info
+    if "Ports" in txt:
+        begin = i + 1
+    if "Active Port" in txt:
+        end = i + 1
+
+ports = ports[begin:end]
+# Select text after : and remove whitespaces
+ports = [''.join(i[:i.find(":")]).split()[0] for i in ports if ":" in i]
+
+# Active port line is the last, so remove from ports list (active port is still in the list after)
+activePort = ports[-1]
+activePort = activePort[activePort.find(":") + 2:]
+ports = ports[:-1]
+
+#print("Active: ", activePort)
+#print("Ports: ", ports)
+
+# Select index of port after active port
+for i, port in enumerate(ports):
+    if port == activePort:
+        i = (i + 1) % len(ports)
+        break
+
+#print(i, len(ports), ports[i], activePort)
+
+# Change port
+cmd = ["pactl", "set-sink-port", sinkName, ports[i]]
+#print(cmd)
+subprocess.run(cmd)
 
 #|grep \*|cut -c 12")
 
-begin = end = cur = 0
-
-sinks = []
-
-for line in sinkStr:
-    if "index" in line:
-        end = cur
-        if end != 0:
-            sinks.append([sinkStr[i] for i in range(begin, end)])
-        begin = cur
-    cur += 1
-end = cur
-
-# Catch last sink
-sinks.append([sinkStr[i] for i in range(begin, end)])
-
-# Remove line with n sink(s) available
-sinks = sinks[1:]
-for i in range(len(sinks)):
-    sinks[i] = "\n".join(sinks[i])
-
-active = ''
-indexes = []
-names = []
-# Find active index, make list of indexes and a list of names
-for i in sinks:
-    for line in i.split("\n"):
-        if "index" in line:
-            # Don't use HDMI
-            #if line[-1] == '3':
-            #    break
-            if "*" in line:
-                active = line.split(' ')[-1]
-            print("line: ", line)
-            indexes.append(line.split(' ')[-1])
-        if "name:" in line:
-            names.append(line[line.find('<') + 1:line.find('>')])
-
-print("Indexes: ", indexes)
-nextIndex = 0
-
-# Chooses next index after active
-for i in range(len(indexes)):
-    if indexes[i] == active:
-        nextIndex = (i + 1)%(len(indexes))
-
-for i in range(len(names)):
-    s = ""
-    if active == indexes[i]:
-        s += "*"
-    s+= names[i]
-    print(s)
-
-print("Swapping to",names[nextIndex])
-
-print(indexes[nextIndex], active)
-print(" ".join(["pacmd", "set-default-sink", names[nextIndex]]))
-subprocess.run(["pacmd", "set-default-sink", names[nextIndex]])
+#begin = end = cur = 0
+#
+#sinks = []
+#
+#for line in ports:
+#    if "index" in line:
+#        end = cur
+#        if end != 0:
+#            sinks.append([ports[i] for i in range(begin, end)])
+#        begin = cur
+#    cur += 1
+#end = cur
+#
+## Catch last sink
+#sinks.append([ports[i] for i in range(begin, end)])
+#
+## Remove line with n sink(s) available
+#sinks = sinks[1:]
+#for i in range(len(sinks)):
+#    sinks[i] = "\n".join(sinks[i])
+#
+#active = ''
+#indexes = []
+#names = []
+## Find active index, make list of indexes and a list of names
+#for i in sinks:
+#    for line in i.split("\n"):
+#        if "index" in line:
+#            # Don't use HDMI
+#            #if line[-1] == '3':
+#            #    break
+#            if "*" in line:
+#                active = line.split(' ')[-1]
+#            print("line: ", line)
+#            indexes.append(line.split(' ')[-1])
+#        if "name:" in line:
+#            names.append(line[line.find('<') + 1:line.find('>')])
+#
+#print("Indexes: ", indexes)
+#nextIndex = 0
+#
+## Chooses next index after active
+#for i in range(len(indexes)):
+#    if indexes[i] == active:
+#        nextIndex = (i + 1)%(len(indexes))
+#
+#for i in range(len(names)):
+#    s = ""
+#    if active == indexes[i]:
+#        s += "*"
+#    s+= names[i]
+#    print(s)
+#
+#print("Swapping to",names[nextIndex])
+#
+#print(indexes[nextIndex], active)
+#print(" ".join(["pacmd", "set-default-sink", names[nextIndex]]))
+#subprocess.run(["pacmd", "set-default-sink", names[nextIndex]])
