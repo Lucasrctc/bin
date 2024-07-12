81bd9849b36689060a49c35421f3cf2aaa313ad1
Lucas <lucasrctc@gmail.com>
2024-07-12 14:02:20 -0500

    more user-friendly hpcc script

--- hpcc.sh
+++ hpcc.sh
@@ -2,15 +2,18 @@
 
 set timeout -1
 
-set brachiosaurus "NQBGBLFJ4mzvH57yrK5vEyqSz\r"
+#Trocar pela sua senha, mas mantem o \r no final
+set senha "NQBGBLFJ4mzvH57yrK5vEyqSz\r"
 
-set stegosaurus "luccorte\r"
+#Mesmo aqui, trocar pelo seu usuario e manter o \r
+set eRaider "luccorte\r"
 
+#Trocar luccorte aqui pelo seu usuario
 spawn ssh luccorte@ssh.ttu.edu
 
 expect "*assword: " 
 
-send -- $brachiosaurus
+send -- $senha
 
 expect "SSH instance: " 
 
@@ -18,18 +21,23 @@ send -- "login.hpcc.ttu.edu\r"
 
 expect "*?ser account to connect with: " 
 
-send -- $stegosaurus 
+send -- $eRaider 
 
 expect "*?assword: " 
 
-send -- $brachiosaurus 
+send -- $senha 
 
 expect "*Current Storage * " 
 
+#Eu normalmente logo numa sessao interativa pra poder usar o terminal sem medo de travar o login node:
 #send -- "interactive -p nocona\r"
 
+#Ou logo no nocona como acima ou logo no matador:
+#send -- "interactive -p matador\r"
+
+#Se quiser ver como esta a situacao da fila assim que logar, descomenta essas 2 linhas abaixo
 #expect "* ready for job*"
 
-#send -- "cd Baryonyx/python;squeue|grep matador\r"
+#send -- "squeue|grep matador\r"
 
 interact
